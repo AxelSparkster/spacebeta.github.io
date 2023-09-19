27a854250e07d8a716b9c4ba21d37219ec1a7e44
Axel Sparkster <axelsparkster@gmail.com>
2023-09-19 09:41:12 -0500

    feat: add toolbar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import BookIcon from '@mui/icons-material/Book';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import ViewKanbanIcon from '@mui/icons-material/ViewKanban';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit">Home</Button>
+          <Button color="inherit">About</Button>
+          <Button color="inherit">Social</Button>
+
+          <Box sx={{ flexGrow: 1 }}/>
+
+          <IconButton href="https://blog.shootinginto.space/" color="inherit" aria-label="menu">
+            <BookIcon />
+          </IconButton>
+          <IconButton href="https://twitter.com/AxelSparkster" color="inherit" aria-label="menu">
+            <TwitterIcon />
+          </IconButton>
+          <IconButton href="https://github.com/AxelSparkster" color="inherit" aria-label="menu">
+            <GitHubIcon />
+          </IconButton>
+          <IconButton href="https://trello.com/b/CtYfAAPA/shootingintospace" color="inherit" aria-label="menu">
+            <ViewKanbanIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
 
