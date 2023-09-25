2026333d226369845b7df17b28312f206c935b8f
Axel Sparkster <axelsparkster@gmail.com>
2023-09-25 10:32:06 -0500

    feat: add centered card

--- src/App.js
+++ src/App.js
@@ -8,12 +8,14 @@ import BookIcon from '@mui/icons-material/Book';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import ViewKanbanIcon from '@mui/icons-material/ViewKanban';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
 
 function App() {
   return (
     <Box style={{ height: "100vh" }} sx={{ flexGrow: 1 }}>
-      <AppBar position="fixed">
-        <Toolbar>
+      <AppBar>
+        <Toolbar position="sticky">
           <Button color="inherit">Home</Button>
           <Button color="inherit">About</Button>
           <Button color="inherit">Social</Button>
@@ -34,8 +36,14 @@ function App() {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <Box style={{ backgroundImage: "url(/img/bg1.jpg)", backgroundSize: "cover", height: "100vh" }} sx={{ flexGrow: 1 }}>
-        Hello World
+      <Toolbar/>
+      <Box style={{ backgroundImage: "url(/img/bg1.jpg)", backgroundSize: "cover",
+      height: "100vh"}} sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Card sx={{ maxWidth: 275 }}>
+          <CardContent>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </CardContent>
+        </Card>
       </Box>
     </Box>
   );
