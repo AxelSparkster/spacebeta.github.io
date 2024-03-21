cf1d37d3336ed3f7e6d567a59b36772c50adc5ed
Axel Sparkster <axelsparkster@gmail.com>
2024-03-21 01:32:01 -0500

    feat: visual updates to pages

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -3,11 +3,17 @@ import CardContent from '@mui/material/CardContent';
 
 export default function Social(){
     return (
-      <Card sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
-       m: 1, minWidth: 500, maxWidth: 'sm'}}>
-        <CardContent>
-          social
-        </CardContent>
-      </Card>
+        <div className="default-layout">
+            <div>
+                {/* TODO: make this into a component */}
+                {/* TODO: put CSS styles into a file */}
+                <Card sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center',
+                m: 1, minWidth: 400, maxWidth: 400, minHeight: 300, maxHeight: 400}}>
+                    <CardContent className="temp-card">
+                    social
+                    </CardContent>
+                </Card>
+            </div>
+        </div>
     );
   }
