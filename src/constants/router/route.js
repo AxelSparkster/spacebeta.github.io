4d33ddea9a084b4191fc2cd44d08a13abc715782
Axel Sparkster <axelsparkster@gmail.com>
2024-03-21 22:49:47 -0500

    chore: update component locations for route.js

--- src/constants/router/route.js
+++ src/constants/router/route.js
@@ -1,8 +1,8 @@
 import { Route } from 'react-router-dom';
-import Home from "../../components/Home";
-import About from "../../components/About";
-import Social from "../../components/Social";
-import NoMatch from "../../components/NoMatch";
+import Home from "../../components/Home/Home";
+import About from "../../components/About/About";
+import Social from "../../components/Social/Social";
+import NoMatch from "../../components/NoMatch/NoMatch";
 import Layout from "../../components/Layout";
 
 export const ROUTES = [
